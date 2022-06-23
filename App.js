53503e15cc7a61457572b02a22fce40e180e4a43
hyeja <21800799@handong.edu>
2022-06-23 16:05:11 +0900

    pull dev

--- App.js
+++ App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import store from './src/store/store';
+import QnaNavigation from './src/screens/navigation/QnaNavigation';
 
 const App = () => {
   return (
-    <ScrollView>
-      <Text>Some text</Text>
-      <View>
-        <Text>Some more text</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{ width: 200, height: 200 }}
-        />
-      </View>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        defaultValue="You can type in me"
-      />
-    </ScrollView>
+    <Provider store={store}>
+      <QnaNavigation />
+    </Provider>
   );
-}
+};
 
 export default App;
