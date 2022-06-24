c11b67be45ddeeb8438698bf9e8cb2c383157b93
f-exuan21 <f.exuan21@gmail.com>
2022-06-24 09:34:10 +0900

    saga setting

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Provider } from "react-redux";
 import store from './src/store/store';
 import { View, Text, Image, ScrollView, TextInput, SafeAreaView, StyleSheet, Button } from 'react-native';
 import FullNavigation from './src/screens/navigation/FullNavigation';
 
 const App = () => {
-  return (
-	<Provider store={store}>
-		<SafeAreaView style={styles.container}>
-			<View style={styles.container}>
-				<FullNavigation/>
-			</View>
-		</SafeAreaView>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<SafeAreaView style={styles.container}>
+				<View style={styles.container}>
+					<FullNavigation/>
+				</View>
+			</SafeAreaView>
+		</Provider>
+	);
 };
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}
+});
 
 export default App;
