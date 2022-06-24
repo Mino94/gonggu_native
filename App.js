b2b90722081d9d61d7b818ccd5cded35be1c710b
hyeja <21800799@handong.edu>
2022-06-24 17:40:46 +0900

    6/24 commit

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import store from './src/store/store';
-import QnaNavigation from './src/screens/navigation/QnaNavigation';
+import MainNavigation from './src/screens/navigation/MainNavigation';
+import { Provider } from "react-redux";
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <QnaNavigation />
-    </Provider>
+<Provider store={store}>
+    <SafeAreaView style={styles.container}>
+		<View style={styles.container}>
+			<MainNavigation></MainNavigation>
+		</View>
+	</SafeAreaView>
+</Provider>
   );
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}
+});
 
 export default App;
